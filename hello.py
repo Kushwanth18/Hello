4e21755b05ead1d22f61b90cfe79964fbbdd68dc
Kushwanth <chkush18@gmail.com>
2023-09-10 09:59:32 +0530

    cowsay

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
+import cowsay
+
 print("Hello World!")
 name = input("What is your name?")
 print("Hello ",name)
 age = int(input("What is your age?"))
 print("You are {} years old".format(age))
-print("Hello Dawg!")
-print("How are you)
+cowsay.cow("Hello Dawg!")
+print("How are you")
